b851c429d826a47ccd05447a1f68c9683685382c
joy2362 <abdullahzahidjoy@gmail.com>
2023-09-19 00:25:43 +0600

    feat: header added

--- resources/js/admin/views/auth/login.vue
+++ resources/js/admin/views/auth/login.vue
@@ -2,60 +2,63 @@
   <v-app>
     <GlobalLoading/>
     <div class="main-bg">
-        <h1>
-            asdf
-        </h1>
-      <v-card
-          class="card-login"
-          elevation="2"
-      >
-        <v-card-title class="d-flex justify-center">
-          <img :src="getLogo" alt="Logo" class="login-logo">
-        </v-card-title>
-
-        <v-form
-            class="form-width"
-            @submit.prevent="submit"
-        >
-
-          <div class="form-login">
-            <v-text-field
-                v-model="login.email"
-                :error="!!errors.email"
-                :error-messages="errors.email"
-                color="primary"
-                density="compact"
-                label="Email"
-                type="email"
-                variant="underlined"
-            ></v-text-field>
-
-            <v-text-field
-                v-model="login.password"
-                :append-icon="showBtn.loginPass ? 'mdi-eye' : 'mdi-eye-off'"
-                :error="!!errors.password"
-                :error-messages="errors.password"
-                :type="showBtn.loginPass ? 'text' : 'password'"
-                color="primary"
-                density="compact"
-                label="Password"
-                variant="underlined"
-                @click:append="showBtn.loginPass = !showBtn.loginPass"
-            ></v-text-field>
-            <v-btn :to="{path: '/admin/forget-password'}" plain>Forgot Password?</v-btn>
-          </div>
-          <div class="login-button">
-
-            <v-btn
-                class="ma-2"
-                color="primary"
-                type="submit"
+        <div>
+            <h2 class="text-center mb-4">
+                Welcome to Automation of loan management system
+            </h2>
+            <v-card
+                class="card-login"
+                elevation="2"
             >
-              Login
-            </v-btn>
-          </div>
-        </v-form>
-      </v-card>
+                <v-card-title class="d-flex justify-center">
+                    <img :src="getLogo" alt="Logo" class="login-logo">
+                </v-card-title>
+
+                <v-form
+                    class="form-width"
+                    @submit.prevent="submit"
+                >
+
+                    <div class="form-login">
+                        <v-text-field
+                            v-model="login.email"
+                            :error="!!errors.email"
+                            :error-messages="errors.email"
+                            color="primary"
+                            density="compact"
+                            label="Email"
+                            type="email"
+                            variant="underlined"
+                        ></v-text-field>
+
+                        <v-text-field
+                            v-model="login.password"
+                            :append-icon="showBtn.loginPass ? 'mdi-eye' : 'mdi-eye-off'"
+                            :error="!!errors.password"
+                            :error-messages="errors.password"
+                            :type="showBtn.loginPass ? 'text' : 'password'"
+                            color="primary"
+                            density="compact"
+                            label="Password"
+                            variant="underlined"
+                            @click:append="showBtn.loginPass = !showBtn.loginPass"
+                        ></v-text-field>
+                        <v-btn :to="{path: '/admin/forget-password'}" plain>Forgot Password?</v-btn>
+                    </div>
+                    <div class="login-button">
+
+                        <v-btn
+                            class="ma-2"
+                            color="primary"
+                            type="submit"
+                        >
+                            Login
+                        </v-btn>
+                    </div>
+                </v-form>
+            </v-card>
+        </div>
+
     </div>
   </v-app>
 </template>
